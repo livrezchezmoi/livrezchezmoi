3fea9c79392a7f8371946989e1732e6a49f6a926
Livrezchezmoi <livrezchezmoi@gmail.com>
2016-10-04 10:10:15 +0200

    add prestashop site to git folder

---
+++ config/settings.inc.php
@@ -0,0 +1,16 @@
+<?php
+define('_DB_SERVER_', 'localhost');
+define('_DB_NAME_', 'prestashop');
+define('_DB_USER_', 'prestashop');
+define('_DB_PASSWD_', 'oss');
+define('_DB_PREFIX_', 'ps_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
+define('_PS_CACHE_ENABLED_', '0');
+define('_COOKIE_KEY_', 'HSnTBr9xjQBN2LyeRQmSYMhdt2jCzsbf57q4yH8FYjVroMBHAk7HDijF');
+define('_COOKIE_IV_', 'PYmIUkwY');
+define('_PS_CREATION_DATE_', '2016-10-03');
+if (!defined('_PS_VERSION_'))
+	define('_PS_VERSION_', '1.6.1.7');
+define('_RIJNDAEL_KEY_', 'CKJYUOGekGrXSOiqp9OmInKW3gSdu4yo');
+define('_RIJNDAEL_IV_', 'POAixTGWA8qdahg5iHHFIw==');
